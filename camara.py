144fd5d2c0e5bad05bcefd551aa4cee126398c1c
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2022-12-11 17:00:03 -0600

    pruebas login_facial

--- camara.py
+++ camara.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 # Si tienes varias cámaras puedes acceder a ellas en 1, 2, etcétera (en lugar de 0)
 camara = cv2.VideoCapture(0)
 
+res_bd = {"id": 0, "affected": 0}
+
 color_success = "\033[1;32;40m"
 color_error = "\033[1;31;40m"
 color_normal = "\033[0;37;40m"
@@ -71,7 +73,76 @@ def register_capture():
         return render_template("camara.html")
     
 
+# LOGIN #
+def compatibility(img1, img2):
+    orb = cv2.ORB_create()
 
+    kpa, dac1 = orb.detectAndCompute(img1, None)
+    kpa, dac2 = orb.detectAndCompute(img2, None)
+
+    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = comp.match(dac1, dac2)
+
+    similar = [x for x in matches if x.distance < 70]
+    if len(matches) == 0:
+        return 0
+    return len(similar)/len(matches)
+
+@app.route("/tomar_foto_descargar",methods=["GET", "POST"])
+def login_capture():
+    msg = ''
+    cap = camara
+    user_login = request.form['login']
+    img = f"{user_login}_login.jpg"
+    img_user = f"{user_login}.jpg"
+
+    #while True:
+        #ret, frame = cap.read()
+        #cv2.imshow("Login Facial", frame)
+        #if cv2.waitKey(1) == 27:
+            #break
+    
+    ret, frame = cap.read()
+    cv2.imwrite(img, frame)
+    cap.release()
+    #cv2.destroyAllWindows()
+
+    #user_entry2.delete(0, END)
+    
+    pixels = plt.imread(img)
+    faces = MTCNN().detect_faces(pixels)
+
+    
+    if face(img, faces) == True:
+        msg = "INGRESO CORRECTO"
+        return render_template("camara.html",msg=msg)
+    else:
+        return render_template("camara.html")
+    #getEnter(screen2)
+
+    res_db = db.getUser(user_login, img_user)
+    if(res_db["affected"]):
+        my_files = os.listdir()
+        if img_user in my_files:
+            face_reg = cv2.imread(img_user, 0)
+            face_log = cv2.imread(img, 0)
+
+            comp = compatibility(face_reg, face_log)
+            
+            if comp >= 0.94:
+                print("{}Compatibilidad del {:.1%}{}".format(color_success, float(comp), color_normal))
+                print(f"Bienvenido, {user_login}", 1)
+            else:
+                print("{}Compatibilidad del {:.1%}{}".format(color_error, float(comp), color_normal))
+                print("¡Error! Incopatibilidad de datos", 0)
+            os.remove(img_user)
+    
+        else:
+            print("¡Error! Usuario no encontrado", 0)
+    else:
+        print("¡Error! Usuario no encontrado", 0)
+    os.remove(img)
 
 
 
@@ -108,7 +179,7 @@ def streaming_camara():
 
 
 # Cuando toman la foto
-@app.route("/tomar_foto_descargar")
+#@app.route("/tomar_foto_descargar")
 def descargar_foto():
     ok, frame = obtener_frame_camara()
     if not ok:
