6d76da332e7906d4fd6db0cef55098e70a08c97d
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2022-12-11 20:16:35 -0600

    prueba login facial 3

--- pruebaqr.py
+++ pruebaqr.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, redirect, url_for, flash,session
+from flask import Flask,render_template, request, redirect, url_for, flash,session,Response,jsonify
 from flask_mysqldb import MySQL, MySQLdb
 import re
 import controlador
@@ -10,8 +10,12 @@ import os
 from os import path #pip install notify-py
 from notifypy import Notify
 import sys
-
-
+import uuid
+import os
+import cv2
+from matplotlib import pyplot as plt
+from mtcnn.mtcnn import MTCNN
+import database as db
 
 #https://github.com/Daniela8426/App_Citas/blob/main/app.py LOGIN
 
@@ -232,10 +236,171 @@ def creacionQR():
     else:
         return render_template("ppp.html",msg=msg)
         
-    
-    
-   
+#LOGIN Y REGISTRO FACIAL
 
+
+camara = cv2.VideoCapture(0)
+
+res_bd = {"id": 0, "affected": 0}
+
+color_success = "\033[1;32;40m"
+color_error = "\033[1;31;40m"
+color_normal = "\033[0;37;40m"
+
+def face(img, faces):
+    data = plt.imread(img)
+    for i in range(len(faces)):
+        x1, y1, ancho, alto = faces[i]["box"]
+        x2, y2 = x1 + ancho, y1 + alto
+        plt.subplot(1,len(faces), i + 1)
+        plt.axis("off")
+        face = cv2.resize(data[y1:y2, x1:x2],(150,200), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(img, face)
+        plt.imshow(data[y1:y2, x1:x2])
+        
+# REGISTER #
+def register_face_db(img):
+    name_user = img.replace(".jpg","").replace(".png","")
+    res_bd = db.registerUser(name_user, img)
+
+    #getEnter(screen1)#
+    if(res_bd["affected"]== True):
+        print("¡Éxito! Se ha registrado correctamente", 1)#
+        return render_template("camara.html")
+    else:
+        print("¡Error! No se ha registrado correctamente", 0)#
+        return render_template("camara.html")
+    os.remove(img)
+    
+    
+@app.route("/tomar_foto_guardar", methods=["GET", "POST"])    
+def register_capture():
+    cap = camara
+    user_reg_img = request.form['usuario']
+    img = f"{user_reg_img}.jpg"
+
+    ret, frame = cap.read()
+    cv2.imwrite(img, frame)
+    cap.release()
+    
+    pixels = plt.imread(img)
+    faces = MTCNN().detect_faces(pixels)
+    face(img, faces)
+    if register_face_db(img) == True:
+        return render_template("camara.html")
+    else:
+        return render_template("camara.html")
+    
+
+# LOGIN #
+def compatibility(img1, img2):
+    orb = cv2.ORB_create()
+
+    kpa, dac1 = orb.detectAndCompute(img1, None)
+    kpa, dac2 = orb.detectAndCompute(img2, None)
+
+    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    matches = comp.match(dac1, dac2)
+
+    similar = [x for x in matches if x.distance < 70]
+    if len(matches) == 0:
+        return 0
+    return len(similar)/len(matches)
+
+@app.route("/tomar_foto_login",methods=["GET", "POST"])
+def login_capture():
+    msg = ''
+    cap = camara
+    user_login = request.form['login']
+    img = f"{user_login}_login.jpg"
+    img_user = f"{user_login}.jpg"
+
+    ret, frame = cap.read()
+    cv2.imwrite(img, frame)
+    cap.release()
+    
+    pixels = plt.imread(img)
+    faces = MTCNN().detect_faces(pixels)
+
+    
+    face(img, faces) 
+
+
+    res_db = db.getlogin(user_login, img_user)
+    if(res_db["affected"]):
+        my_files = os.listdir()
+        if img_user in my_files:
+            face_reg = cv2.imread(img_user, 0)
+            face_log = cv2.imread(img, 0)
+
+            comp = compatibility(face_reg, face_log)
+            
+            if comp >= 0.94:
+                print("{}Compatibilidad del {:.1%}{}".format(color_success, float(comp), color_normal))
+                print(f"Bienvenido, {user_login}", 1)
+                return render_template("camara.html")
+            else:
+                print("{}Compatibilidad del {:.1%}{}".format(color_error, float(comp), color_normal))
+                print("¡Error! Incopatibilidad de datos", 0)
+                return render_template("camara.html")
+            os.remove(img_user)
+    
+        else:
+            print("¡Error! Usuario no encontrado", 0)
+    else:
+        print("¡Error! Usuario no encontrado", 0)
+    os.remove(img)
+
+
+def generador_frames():
+    while True:
+        ok, imagen = obtener_frame_camara()
+        if not ok:
+            break
+        else:
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + imagen + b"\r\n"
+
+
+def obtener_frame_camara():
+    ok, frame = camara.read()
+    if not ok:
+        return False, None
+    _, bufer = cv2.imencode(".jpg", frame)
+    imagen = bufer.tobytes()
+    return True, imagen
+
+
+# Cuando visiten la ruta
+@app.route("/streaming_camara")
+def streaming_camara():
+    return Response(generador_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# Cuando toman la foto
+#@app.route("/tomar_foto_descargar")
+def descargar_foto():
+    ok, frame = obtener_frame_camara()
+    if not ok:
+        abort(500)
+        return
+    respuesta = Response(frame)
+    respuesta.headers["Content-Type"] = "image/jpeg"
+    respuesta.headers["Content-Transfer-Encoding"] = "Binary"
+    respuesta.headers["Content-Disposition"] = "attachment; filename=\"foto.jpg\""
+    return respuesta
+
+
+#@app.route("/tomar_foto_guardar")
+def guardar_foto():
+    nombre_foto = str(uuid.uuid4()) + ".jpg"
+    ok, frame = camara.read()
+    if ok:
+        cv2.imwrite(nombre_foto, frame)
+    return jsonify({
+        "ok": ok,
+        "nombre_foto": nombre_foto,
+    })
 
 
 if __name__ == "__main__":
